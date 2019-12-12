83bd80e18143f5cd09789e9526248772c5cac287
Artur <arturlee_word@hotmail.com>
2019-12-12 03:07:17 +0100

    Updates

---
+++ precache-manifest.fb8b75fbd5d69331f227c63da6dd36fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d785794c4bfd42a425c6c44dd515b220",
+    "url": "/github.io/React_Rick-Morty/index.html"
+  },
+  {
+    "revision": "3eaf22e9b0cbe5f3c195",
+    "url": "/github.io/React_Rick-Morty/static/css/main.9c0c5ccf.chunk.css"
+  },
+  {
+    "revision": "e96a370ca5256bb820d9",
+    "url": "/github.io/React_Rick-Morty/static/js/2.06a5cb95.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/github.io/React_Rick-Morty/static/js/2.06a5cb95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3eaf22e9b0cbe5f3c195",
+    "url": "/github.io/React_Rick-Morty/static/js/main.a2c3ba56.chunk.js"
+  },
+  {
+    "revision": "062a4fd067716dd99802",
+    "url": "/github.io/React_Rick-Morty/static/js/runtime-main.58303dd2.js"
+  }
+]);
